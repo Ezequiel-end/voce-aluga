26439404fcec92c4f6121d3c2e17e2d63fd83cf0
Ezequiel-end <ezequiel.es609@gmail.com>
2025-06-20 22:10:47 -0300

    Update EstoqueRepository.java

--- src/main/java/com/vocealuga/dao/EstoqueRepository.java
+++ src/main/java/com/vocealuga/dao/EstoqueRepository.java
@@ -4,6 +4,10 @@ import com.vocealuga.model.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EstoqueRepository extends JpaRepository<Estoque, Integer> {
+
+    Optional<Estoque> findByVeiculoIdAndFilialId(Integer veiculoId, Integer filialId);
 }
