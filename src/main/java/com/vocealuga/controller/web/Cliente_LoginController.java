c8c3a25d3a2aa9a312955054f4834a3f2fa5e9f1
Gabriel <gsmainiere1901@gmail.com>
2025-06-28 12:44:59 -0300

    fixing layout login customer

--- src/main/java/com/vocealuga/controller/web/Cliente_LoginController.java
+++ src/main/java/com/vocealuga/controller/web/Cliente_LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import jakarta.servlet.http.HttpSession;
 import java.util.Optional;
 
@@ -18,13 +20,21 @@ public class Cliente_LoginController {
 
     // Removido FuncionarioService, pois o login de funcionário agora está em FuncionarioLoginController
 
-    @GetMapping("/login")
+    /*@GetMapping("/login")
     public String showLoginForm(Model model) {
         model.addAttribute("cliente", new Cliente());
         return "cliente/login";
+    }*/
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        if (!model.containsAttribute("cliente")) {
+            model.addAttribute("cliente", new Cliente());
+        }
+        return "cliente/login";
     }
 
-    @PostMapping("/login")
+    /*@PostMapping("/login")
     public String processLogin(@RequestParam String email, @RequestParam String senha, Model model, HttpSession session) {
         Optional<Cliente> cliente = clienteService.login(email, senha);
 
@@ -32,9 +42,25 @@ public class Cliente_LoginController {
             session.setAttribute("loggedInClient", cliente.get());
             return "redirect:/cliente/dashboard"; // Redireciona para o dashboard do cliente
         } else {
-            model.addAttribute("erro", "E-mail ou senha inválidos.");
+            model.addAttribute("error", "E-mail ou senha inválidos.");
             model.addAttribute("cliente", new Cliente()); // Mantém o objeto cliente para o formulário
             return "cliente/login"; // Volta para a página de login do cliente com erro
+        }
+    } */
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String email,
+                            @RequestParam String senha,
+                            HttpSession session,
+                            RedirectAttributes redirectAttributes) {
+        Optional<Cliente> cliente = clienteService.login(email, senha);
+
+        if (cliente.isPresent()) {
+            session.setAttribute("loggedInClient", cliente.get());
+            return "redirect:/cliente/dashboard";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "E-mail ou senha inválidos.");
+            return "redirect:/cliente/login";
         }
     }
 }
